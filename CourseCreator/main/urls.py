9aa3600bfa90a4559a06f21b8048a477bbdb938e
Angelina112358 <angelma112358@gmail.com>
2022-06-05 04:27:19 +0300

    Added style on Main page Added catalog and home links

--- CourseCreator/main/urls.py
+++ CourseCreator/main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('<int:id>/<str:slug>', views.product_detail, name='product_detail'),
     path('<str:category_slug>', views.product_list, name='product_list_by_category'),
-    path('', views.product_list, name='product_list')
+    path('catalog/', views.product_list, name='product_list')
 ]
